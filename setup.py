7ee024ae38c1f5314e8860a177dee33a5440f618
Jannis Leidel <jannis@leidel.info>
2009-02-04 04:12:09 +0100

    Initial checkin, version 0.1

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+long_desciption = """
+A plugin for setuptools to find files under Mercurial version control to be
+automatically included as package data.
+
+It works correctly with Mercurial 1.x and uses Mercurial's Python library by
+default. It will however fall back to use the command line programm hg(1) to
+determin the list of files.
+"""
+
+setup(
+    name="setuptools_hg",
+    version='0.1',
+    author="Jannis Leidel",
+    author_email="jannis@leidel.info",
+    url="http://bitbucket.org/jezdez/",
+    description="Setuptools plugin for finding files under Mercurial version control.",
+    long_desciption=long_desciption,
+    license="BSD License",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Version Control"
+    ],
+    py_modules=["setuptools_hg"],
+    entry_points = {
+        "setuptools.file_finders": [
+            "hg = setuptools_hg:hg_file_finder"
+        ]
+    }
+)
---
+++ setuptools_hg.py
@@ -0,0 +1,80 @@
+"""
+A plugin for setuptools to find files under the Mercurial version control
+system which uses the Python library by default and falls back to use the
+command line programm hg(1).
+"""
+__version__ = '0.1'
+__author__ = 'Jannis Leidel'
+__all__ = ['hg_file_finder']
+
+import os
+import subprocess
+
+try:
+    from mercurial import hg, ui, cmdutil
+    from mercurial.repo import RepoError
+except ImportError:
+    hg = None
+
+if os.getenv('HG_SETUPTOOLS_FORCE_CMD'):
+    hg = None
+
+def find_files_with_lib(dirname):
+    """
+    Use the Mercurial library to recursively find versioned files in dirname.
+    """
+    try:
+        repo = hg.repository(ui.ui(), dirname)
+    except RepoError:
+        return
+
+    # tuple of (modified, added, removed, deleted, unknown, ignored, clean)
+    modified, added, removed, deleted, unknown = repo.status()[:5]
+
+    # exclude all files that hg knows about, but haven't been added,
+    # or have been deleted, removed, or have an unknown status
+    excluded = removed + deleted + unknown
+
+    rev = None
+    match = cmdutil.match(repo, [], {}, default='relglob')
+    match.bad = lambda x, y: False
+    for abs in repo[rev].walk(match):
+        if not rev and abs not in repo.dirstate:
+            continue
+        if abs in excluded:
+            continue
+        yield abs
+
+def find_files_with_cmd(dirname="."):
+    """
+    Use the hg command to recursively find versioned files in dirname.
+    """
+    proc = subprocess.Popen(['hg', 'locate'],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            cwd=dirname)
+    stdout, stderr = proc.communicate()
+    return stdout.splitlines()
+
+def hg_file_finder(dirname="."):
+    """
+    Find the files in ``dirname`` under Mercurial version control.
+    """
+    if not dirname:
+        dirname = "."
+    if hg is None:
+        return find_files_with_cmd(dirname)
+    return find_files_with_lib(dirname)
+
+def main():
+    import sys
+    from pprint import pprint
+    try:
+        dirname = sys.argv[1]
+    except IndexError:
+        dirname = "."
+    pprint(list(hg_file_finder(dirname)))
+
+if __name__ == '__main__':
+    main()
