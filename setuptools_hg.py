c7f869f69aa15279167dbb74cb01208b77f04f26
Jason R. Coombs <jaraco@jaraco.com>
2010-07-16 22:59:37 -0400

    Substantial refactoring to support class-based context for performing Hg operations

--- setuptools_hg.py
+++ setuptools_hg.py
@@ -10,89 +10,124 @@ __all__ = ['hg_file_finder']
 import os
 import subprocess
 
-try:
-    from mercurial.__version__ import version
-    from mercurial import hg, ui, cmdutil
-except:
-    hg = None
+next = lambda iter: iter.next()
 
-try:
-    from mercurial.repo import RepoError
-except:
-    try:
-        from mercurial.error import RepoError
-    except:
-        pass
+class HGRepoManager(object);
+	def __init__(self, location='.'):
+		self.location = location
+		self.do_imports()
 
-OLD_VERSIONS = ('1.0', '1.0.1', '1.0.2')
+	def is_valid(self):
+		return True
 
-if os.environ.get('HG_SETUPTOOLS_FORCE_CMD', False):
-    hg = None
+	def do_imports(self):
+		pass
 
-def find_files_with_cmd(dirname="."):
-    """
-    Use the hg command to recursively find versioned files in dirname.
-    """
-    try:
-        proc = subprocess.Popen(['hg', 'locate'],
-                                stdin=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                cwd=dirname)
-        stdout, stderr = proc.communicate()
-    except:
-        # Let's behave a bit nicer and return nothing if something fails.
-        return []
-    return stdout.splitlines()
+	@staticmethod
+	def get_valid_managers(location):
+		classes = (LibraryManager, LegacyLibraryManager, SubprocessManager)
+		managers = (cls(location) for cls in classes)
+		return (mgr for mgr in managers if mgr.is_valid())
 
-def find_files_with_lib(dirname):
-    """
-    Use the Mercurial library to recursively find versioned files in dirname.
-    """
-    try:
-        try:
-            repo = hg.repository(ui.ui(), path=dirname)
-        except RepoError:
-            return
-        # tuple of (modified, added, removed, deleted, unknown, ignored, clean)
-        modified, added, removed, deleted, unknown = repo.status()[:5]
+class SubprocessManager(HGRepoManager):
+	exe = 'hg'
 
-        # exclude all files that hg knows about, but haven't been added,
-        # or have been deleted, removed, or have an unknown status
-        excluded = removed + deleted + unknown
+	def is_valid(self):
+		return subprocess.call([self.exe, 'version']) == 0
 
-        if version in OLD_VERSIONS:
-            from mercurial import util
-            node = None
-            for src, abs, rel, exact in cmdutil.walk(repo, [], {}, node=node,
-                                    badmatch=util.always, default='relglob'):
-                if src == 'b':
-                    continue
-                if not node and abs not in repo.dirstate:
-                    continue
-                if abs in excluded:
-                    continue
-                yield abs
-        else:
-            rev = None
-            match = cmdutil.match(repo, [], {}, default='relglob')
-            match.bad = lambda x, y: False
-            for abs in repo[rev].walk(match):
-                if not rev and abs not in repo.dirstate:
-                    continue
-                if abs in excluded:
-                    continue
-                yield abs
-    except:
-        # try calling hg command as a last resort
-        find_files_with_cmd(dirname)
+	def find_files():
+		"""
+		Use the hg command to recursively find versioned files in dirname.
+		"""
+		try:
+			proc = subprocess.Popen(
+				['hg', 'locate'],
+				stdout=subprocess.PIPE,
+				cwd=self.location,
+				)
+			stdout, stderr = proc.communicate()
+		except:
+			# Let's behave a bit nicer and return nothing if something fails.
+			return []
+		return stdout.splitlines()
+
+
+class LibraryManager(HGManager):
+	OLD_VERSIONS = ('1.0', '1.0.1', '1.0.2')
+
+	def do_imports(self):
+		try:
+			from mercurial.__version__ import version
+			from mercurial import hg, ui, cmdutil
+		except ImportError:
+			pass
+
+		try:
+			from mercurial import error
+		except ImportError:
+			# mercurial < 1.2
+			from mercurial import repo as error
+		RepoError = error.RepoError
+		del self
+		del error
+		globals.update(vars())
+
+	def is_valid(self):
+		os.environ.get('HG_SETUPTOOLS_FORCE_CMD', False)
+		return not force_cmd and 'hg' in globals() and self.version_match()
+
+	def version_match(self):
+		return version not in self.OLD_VERSIONS
+
+	def _get_repo(self):
+		return hg.repository(ui.ui(), path=self.location)
+
+	def _get_excluded(self, repo):
+		"""
+		Return all files that hg knows about, but haven't been added,
+		deleted, or removed or have an unknown status.
+		"""
+		modified, added, removed, deleted, unknown = repo.status()[:5]
+		return removed + deleted + unknown
+
+	def find_files(self):
+		"""
+		Use the Mercurial library to recursively find versioned files in dirname.
+		"""
+		repo = self._get_repo()
+		excluded = self._get_excluded()
+		rev = None
+		match = cmdutil.match(repo, [], {}, default='relglob')
+		match.bad = lambda x, y: False
+		return (abs
+			for abs in repo[rev].walk(match)
+			if rev and abs in repo.dirstate and abs not in excluded
+			)
+
+class LegacyLibraryManager(LibraryManager):
+	def version_match(self):
+		return version in self.OLD_VERSIONS
+
+	def find_files(self):
+		repo = self._get_repo()
+		excluded = self._get_excluded()
+		from mercurial import util
+		node = None
+		walker = cmdutil.walk(repo, [], {}, node=node,
+			badmatch=util.always, default='relglob')
+		return (abs
+			for src, abs, rel, exact in walker
+			if src != 'b' and (node or abs in repo.dirstate)
+			and abs not in excluded
+			)
 
 def hg_file_finder(dirname="."):
     """
     Find the files in ``dirname`` under Mercurial version control.
     """
-    if not dirname:
-        dirname = "."
-    if hg is None:
-        return find_files_with_cmd(dirname)
-    return find_files_with_lib(dirname)
+    dirname = dirname or '.'
+    for mgr in HGRepoManager.get_valid_managers(dirname):
+		try:
+			return mgr.find_files()
+		except:
+			pass
