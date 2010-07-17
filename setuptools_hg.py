d8c7ce5a9420e2bafd7373ccbe4c47e12804af14
Jason R. Coombs <jaraco@jaraco.com>
2010-07-17 09:56:05 -0400

    Fixed a number of errors - the classes now work in my environment

--- setuptools_hg.py
+++ setuptools_hg.py
@@ -1,26 +1,27 @@
 """
-A plugin for setuptools to find files under the Mercurial version control
-system which uses the Python library by default and falls back to use the
-command line programm hg(1).
+Classes for working with repositories using the Mercurial version
+control system. Includes a plugin for setuptools to find files managed
+by the version control system.
+
+Classes support using the native Python library interfaces or using the
+hg(1) command as a subprocess.
 """
-__version__ = '0.2'
+__version__ = '0.3'
 __author__ = 'Jannis Leidel'
 __all__ = ['hg_file_finder']
 
 import os
 import subprocess
 
-next = lambda iter: iter.next()
-
 class HGRepoManager(object):
 	def __init__(self, location='.'):
 		self.location = location
-		self.do_imports()
+		self.setup()
 
 	def is_valid(self):
 		return True
 
-	def do_imports(self):
+	def setup(self):
 		pass
 
 	@staticmethod
@@ -29,13 +30,21 @@ class HGRepoManager(object):
 		managers = (cls(location) for cls in classes)
 		return (mgr for mgr in managers if mgr.is_valid())
 
+	def __repr__(self):
+		class_name = self.__class__.__name__
+		loc = self.location
+		return '%(class_name)s(%(loc)r)' % vars()
+
 class SubprocessManager(HGRepoManager):
 	exe = 'hg'
 
 	def is_valid(self):
-		return subprocess.call([self.exe, 'version']) == 0
+		return 0 == subprocess.call(
+			[self.exe, 'version'],
+			stdout=self._get_devnull(),
+			)
 
-	def find_files():
+	def find_files(self):
 		"""
 		Use the hg command to recursively find versioned files in dirname.
 		"""
@@ -51,11 +60,15 @@ class SubprocessManager(HGRepoManager):
 			return []
 		return stdout.splitlines()
 
+	@staticmethod
+	def _get_devnull():
+		return open(os.path.devnull, 'w')
+
 
 class LibraryManager(HGRepoManager):
 	OLD_VERSIONS = ('1.0', '1.0.1', '1.0.2')
 
-	def do_imports(self):
+	def setup(self):
 		try:
 			from mercurial.__version__ import version
 			from mercurial import hg, ui, cmdutil
@@ -63,13 +76,11 @@ class LibraryManager(HGRepoManager):
 			pass
 
 		try:
-			from mercurial import error
+			from mercurial.error import RepoError
 		except ImportError:
 			# mercurial < 1.2
-			from mercurial import repo as error
-		RepoError = error.RepoError
+			from mercurial.repo import RepoError
 		del self
-		del error
 		globals().update(vars())
 
 	def is_valid(self):
@@ -82,26 +93,32 @@ class LibraryManager(HGRepoManager):
 	def _get_repo(self):
 		return hg.repository(ui.ui(), path=self.location)
 
-	def _get_excluded(self, repo):
+	@property
+	def repo(self):
+		if not hasattr(self, '_repo'):
+			self._repo = self._get_repo()
+		return self._repo
+
+	def _get_excluded(self):
 		"""
 		Return all files that hg knows about, but haven't been added,
 		deleted, or removed or have an unknown status.
 		"""
-		modified, added, removed, deleted, unknown = repo.status()[:5]
+		modified, added, removed, deleted, unknown = self.repo.status()[:5]
 		return removed + deleted + unknown
 
 	def find_files(self):
 		"""
 		Use the Mercurial library to recursively find versioned files in dirname.
 		"""
-		repo = self._get_repo()
 		excluded = self._get_excluded()
 		rev = None
-		match = cmdutil.match(repo, [], {}, default='relglob')
+		match = cmdutil.match(self.repo, [], {}, default='relglob')
 		match.bad = lambda x, y: False
 		return (abs
-			for abs in repo[rev].walk(match)
-			if rev and abs in repo.dirstate and abs not in excluded
+			for abs in self.repo[rev].walk(match)
+			if (rev or abs in self.repo.dirstate)
+			and abs not in excluded
 			)
 
 class LegacyLibraryManager(LibraryManager):
@@ -109,11 +126,10 @@ class LegacyLibraryManager(LibraryManager):
 		return version in self.OLD_VERSIONS
 
 	def find_files(self):
-		repo = self._get_repo()
 		excluded = self._get_excluded()
 		from mercurial import util
 		node = None
-		walker = cmdutil.walk(repo, [], {}, node=node,
+		walker = cmdutil.walk(self.repo, [], {}, node=node,
 			badmatch=util.always, default='relglob')
 		return (abs
 			for src, abs, rel, exact in walker
@@ -123,12 +139,16 @@ class LegacyLibraryManager(LibraryManager):
 
 def hg_file_finder(dirname="."):
 	"""
-	Find the files in ``dirname`` under Mercurial version control.
+	Find the files in ``dirname`` under Mercurial version control
+	according to the setuptools spec (see
+	http://peak.telecommunity.com/DevCenter/setuptools#adding-support-for-other-revision-control-systems
+	).
 	"""
+	import distutils.log
 	dirname = dirname or '.'
 	for mgr in HGRepoManager.get_valid_managers(dirname):
 		try:
 			return mgr.find_files()
-		except:
-			pass
+		except BaseException, e:
+			distutils.log.warn("Error in %s: %s", mgr, e)
 	return []
