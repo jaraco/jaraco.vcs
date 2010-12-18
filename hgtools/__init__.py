0bb341ea9eb5b3d79465394ea3be75883c14917a
Jason R. Coombs <jaraco@jaraco.com>
2010-12-18 08:58:50 -0500

    Moved plugins into its own module

--- hgtools/__init__.py
+++ hgtools/__init__.py
@@ -1,99 +1,12 @@
 """
-Classes for working with repositories using the Mercurial version
-control system. Includes a plugin for setuptools to find files managed
-by the version control system.
+Module for the hgtools package
 
-Classes support using the native Python library interfaces or using the
-hg(1) command as a subprocess.
+Credit to Jannis Leidel for setuptools_hg from which hgtools was forked.
 """
-__version__ = '0.6'
-__author__ = 'Jannis Leidel/Jason R. Coombs'
-__all__ = ['file_finder_plugin']
 
-import os
+# kept for backward compatibility - will be removed in 0.7 or 0.8
+from .plugins import (
+	version_calc as version_calc_plugin,
+	file_finder as file_finder_plugin,
+	)
 
-from . import managers
-from .py25compat import namedtuple
-
-def file_finder_plugin(dirname="."):
-	"""
-	Find the files in ``dirname`` under Mercurial version control
-	according to the setuptools spec (see
-	http://peak.telecommunity.com/DevCenter/setuptools#adding-support-for-other-revision-control-systems
-	).
-	"""
-	import distutils.log
-	dirname = dirname or '.'
-	try:
-		for mgr in managers.HGRepoManager.get_valid_managers(dirname):
-			try:
-				return mgr.find_files()
-			except Exception, e:
-				distutils.log.warn("hgtools.%s could not find files: %s", mgr, e)
-	except Exception, e:
-		distutils.log.warn("Unexpected error finding valid managers in hgtools.file_finder_plugin: %s", e)
-	return []
-
-def patch_egg_info(force_hg_version=False):
-	"""
-	A hack to replace egg_info.tagged_version with a wrapped version
-	that will use the mercurial version if indicated.
-	"""
-	from setuptools.command.egg_info import egg_info
-	from pkg_resources import safe_version
-	import functools
-	orig_ver = egg_info.tagged_version
-	@functools.wraps(orig_ver)
-	def tagged_version(self):
-		using_hg_version = (
-			force_hg_version
-			or getattr(self.distribution, 'use_hg_version', False)
-			or getattr(self.distribution, 'use_hg_version_increment',
-				False)
-			)
-		if using_hg_version:
-			result = safe_version(self.distribution.get_version())
-		else:
-			result = orig_ver(self)
-		self.tag_build = result
-		return result
-	egg_info.tagged_version = tagged_version
-
-def calculate_version(options={}):
-	default_increment = options.get('increment')
-	# The version is cached in the tag_build value in setup.cfg (so that
-	#  sdist versions will have a copy of the version as determined at
-	#  the build environment).
-	from ConfigParser import ConfigParser
-	parser = ConfigParser()
-	parser.read('setup.cfg')
-	has_tag_build = (parser.has_section('egg_info')
-		and 'tag_build' in parser.options('egg_info'))
-	if has_tag_build:
-		# a cached version is available, so use it.
-		version = parser.get('egg_info', 'tag_build')
-	else:
-		# We don't have a version stored in tag_build, so calculate
-		#  the version using an HGRepoManager.
-		# for now, force the CMD version, because the library version
-		#  is not implemented.
-		os.environ['HGTOOLS_FORCE_CMD'] = 'True'
-		mgr = managers.HGRepoManager.get_first_valid_manager()
-		version = mgr.get_current_version(default_increment)
-	return version
-
-def version_calc_plugin(dist, attr, value):
-	"""
-	Handler for parameter to setup(use_hg_version=value)
-	"""
-	import distutils.log
-	if not value or not 'hg_version' in attr: return
-	# if the user indicates an increment, use it
-	if 'increment' in attr:
-		distutils.log.warn(
-			"use_hg_version_increment is deprecated and will be removed in 0.7. "
-			"Instead, use `use_hg_version=dict(increment='0.0.1')`")
-		value = {'increment': value}
-	options = value if isinstance(value, dict) else {}
-	dist.metadata.version = calculate_version(options)
-	patch_egg_info()
--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 long_description = open('README').read()
 
-from hgtools import calculate_version, patch_egg_info
+# HGTools uses a special technique for getting the version from
+#  mercurial, because it can't require itself to install itself.
+# Don't use this technique in your project. Instead, follow the
+#  directions in the README or see jaraco.util for an example.
+from hgtools.plugins import calculate_version, patch_egg_info
 patch_egg_info(force_hg_version=True)
 
 setup(
@@ -15,7 +19,7 @@ setup(
     long_description=long_description,
     license="GPL2",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
@@ -26,11 +30,11 @@ setup(
     packages=find_packages(),
     entry_points = {
         "setuptools.file_finders": [
-            "hg = hgtools:file_finder_plugin"
+            "hg = hgtools.plugins:file_finder"
         ],
         "distutils.setup_keywords": [
-            "use_hg_version = hgtools:version_calc_plugin",
-            "use_hg_version_increment = hgtools:version_calc_plugin",
+            "use_hg_version = hgtools.plugins:version_calc",
+            "use_hg_version_increment = hgtools.plugins:version_calc",
         ],
     },
 )
