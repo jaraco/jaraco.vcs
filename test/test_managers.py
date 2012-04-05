b98c1fce7dc7c45dd5a7e436c52df45df5060e31
Jason R. Coombs <jaraco@jaraco.com>
2012-04-05 16:04:35 -0400

    Add tests to ensure queries in subdirectories return only files in those subdirectories

--- test/test_managers.py
+++ test/test_managers.py
@@ -1,4 +1,7 @@
 import os
+import contextlib
+import tempfile
+import shutil
 
 from hgtools import managers
 
@@ -22,3 +25,53 @@ def test_existing_only():
 	mgrs = managers.HGRepoManager.get_valid_managers('/')
 	existing = list(managers.HGRepoManager.existing_only(mgrs))
 	assert not existing
+
+@contextlib.contextmanager
+def tempdir_context():
+	temp_dir = tempfile.mkdtemp()
+	orig_dir = os.getcwd()
+	try:
+		os.chdir(temp_dir)
+		yield temp_dir
+	finally:
+		os.chdir(orig_dir)
+		shutil.rmtree(temp_dir)
+
+@contextlib.contextmanager
+def test_repo():
+	with tempdir_context():
+		mgr = managers.SubprocessManager()
+		mgr._run_cmd([mgr.exe, 'init', 'foo'])
+		os.chdir('foo')
+		os.makedirs('bar')
+		touch('bar/baz')
+		mgr._run_cmd([mgr.exe, 'addremove'])
+		mgr._run_cmd([mgr.exe, 'ci', '-m', 'committed'])
+		yield
+
+
+def touch(filename):
+	with open(filename, 'a'):
+		pass
+
+class TestRelativePaths(object):
+	"""
+	Issue #9 demonstrated that we can inadvertently return too many
+	files when location is not the root of the repo. This test demonstrates
+	that we don't have that problem anymore.
+	"""
+	def test_nested_child(self):
+		with test_repo():
+			test_mgr = managers.SubprocessManager('.')
+			assert test_mgr.find_files() == [os.path.join('bar', 'baz')]
+
+	def test_manager_in_child(self):
+		with test_repo():
+			test_mgr = managers.SubprocessManager('bar')
+			assert test_mgr.find_files() == ['baz']
+
+	def test_current_dir_in_child(self):
+		with test_repo():
+			os.chdir('bar')
+			test_mgr = managers.SubprocessManager('.')
+			assert test_mgr.find_files() == ['baz']
