0ac653dc7210f26c356a3d631d0793af02306aa8
Jason R. Coombs <jaraco@jaraco.com>
2013-03-31 22:13:00 -0400

    Updated LibraryManager to also use the Command class

--- hgtools/managers/library.py
+++ hgtools/managers/library.py
@@ -7,6 +7,7 @@ import collections
 import contextlib
 
 from . import base
+from . import cmd
 
 try:
 	import mercurial.__version__
@@ -65,15 +66,10 @@ def in_process_context(params):
 	res.stdio = stdio
 	res.returncode = proc_res.code
 
-class LibraryManager(base.HGRepoManager):
+class LibraryManager(cmd.Command, base.HGRepoManager):
 	"""
 	An HGRepoManager implemented by invoking the hg command in-process.
 	"""
-	exe = 'hg'
-
-	def is_valid(self):
-		modules_present = 'mercurial' in globals() and self.version_match()
-		return modules_present and super(LibraryManager, self).is_valid()
 
 	def _run_hg(self, *params):
 		"""
@@ -87,25 +83,3 @@ class LibraryManager(base.HGRepoManager):
 		if not result.returncode == 0:
 			raise RuntimeError(stderr.strip() or stdout.strip())
 		return stdout.decode('utf-8')
-
-	def find_root(self):
-		try:
-			return self._run_hg('root').strip()
-		except Exception:
-			pass
-
-	def version_match(self):
-		return True
-
-	def find_files(self):
-		"""
-		Find versioned files in self.location
-		"""
-		all_files = self._run_hg('locate', '-I', '.').splitlines()
-		# now we have a list of all files in self.location relative to
-		#  self.find_root()
-		# Remove the parent dirs from them.
-		from_root = os.path.relpath(self.location, self.find_root())
-		loc_rel_paths = [os.path.relpath(path, from_root)
-			for path in all_files]
-		return loc_rel_paths
