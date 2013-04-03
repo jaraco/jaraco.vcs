626d66f18715be1302d516324c1b021bb6c9581f
Jason R. Coombs <jaraco@jaraco.com>
2013-04-02 21:49:09 -0400

    Catching that Exception didn't elucidate the issue

--- hgtools/plugins.py
+++ hgtools/plugins.py
@@ -34,7 +34,7 @@ def file_finder(dirname="."):
 				e = sys.exc_info()[1]
 				distutils.log.warn("hgtools.%s could not find files: %s",
 					mgr, e)
-	except BaseException:
+	except Exception:
 		e = sys.exc_info()[1]
 		distutils.log.warn("Unexpected error finding valid managers in "
 			"hgtools.file_finder_plugin: %s", e)
