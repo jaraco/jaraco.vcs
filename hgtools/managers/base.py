1f474cd8c6f893f1f2706de5d37aafca4f20fd5d
Jason R. Coombs <jaraco@jaraco.com>
2014-01-30 20:29:05 -0500

    Use itersubclasses, with more robust type handling.

--- hgtools/managers/base.py
+++ hgtools/managers/base.py
@@ -7,6 +7,7 @@ import posixpath
 import itertools
 
 from hgtools import versioning
+from hgtools.util import itersubclasses
 
 class HGRepoManager(versioning.VersionManagement, object):
 	"""
@@ -30,17 +31,7 @@ class HGRepoManager(versioning.VersionManagement, object):
 		Get the valid HGRepoManagers for this location.
 		"""
 		by_priority_attr = lambda c: getattr(c, 'priority', 0)
-
-		def _get_all_subclasses(c):
-			"""
-			Get all superclasses up to "object".
-			"""
-			for sc in c.__subclasses__():
-				yield sc
-				for c in _get_all_subclasses(sc):
-					yield c
-
-		classes = sorted(set(_get_all_subclasses(cls)), key = by_priority_attr,
+		classes = sorted(itersubclasses(cls), key = by_priority_attr,
 			reverse = True)
 		all_managers = (c(location) for c in classes)
 		return (mgr for mgr in all_managers if mgr.is_valid())
