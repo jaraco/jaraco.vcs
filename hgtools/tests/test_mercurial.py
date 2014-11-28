a1082c94762f6d3a4472f3ad43b5658e77d16277
Toshio Kuratomi <toshio@fedoraproject.org>
2014-11-28 12:22:31 -0800

    Make test of get_ancestral_tags more robust in the face of a race condition

--- hgtools/tests/test_mercurial.py
+++ hgtools/tests/test_mercurial.py
@@ -114,8 +114,15 @@ class TestTags(object):
 		to the current revision.
 		"""
 		self._setup_branchy_tags()
-		tag, tip = self.mgr.get_ancestral_tags()
-		assert tag.tag == '1.1'
+		tags = self.mgr.get_ancestral_tags()
+		# Two tags expected, one is the tag we made and the other is tip On
+		# fast hardware, the timestamps for these may be close enough that
+		# mercurial doesn't sort them properly by time.
+		tags = list(tags)
+		assert tags[0].tag in ('1.1', 'tip')
+		assert tags[1].tag in ('1.1', 'tip')
+		assert tags[0].tag != tags[1].tag
+		assert len(tags) == 2
 
 	def test_ancestral_tags_specified(self):
 		"""
