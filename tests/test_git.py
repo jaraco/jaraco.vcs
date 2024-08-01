b10534eba905f3a976d75c0f8c54c3eeeda632cb
Jason R. Coombs <jaraco@jaraco.com>
2024-08-01 10:00:18 -0400

    Clean up cruft in test_git

--- tests/test_git.py
+++ tests/test_git.py
@@ -9,30 +9,25 @@ from jaraco.vcs import cmd, subprocess
 
 def test_subprocess_manager_invalid_when_exe_missing():
     """
-    If the hg executable dosen't exist, the manager should report
+    If the executable dosen't exist, the manager should report
     False for .is_valid().
     """
     non_existent_exe = '/non_existent_executable'
     assert not os.path.exists(non_existent_exe)
-    mgr = subprocess.Git()
-    mgr.exe = non_existent_exe
-    assert not mgr.is_valid()
+    repo = subprocess.Git()
+    repo.exe = non_existent_exe
+    assert not repo.is_valid()
 
 
 @pytest.mark.usefixtures("git_repo")
 class TestTags:
-    def setup_method(self, method):
-        self.mgr = vcs.Git('.')
-
-    def teardown_method(self, method):
-        del self.mgr
-
     def test_single_tag(self):
-        assert self.mgr.get_tags() == set()
-        self.mgr._invoke('tag', '-am', "Tagging 1.0", '1.0')
-        assert self.mgr.get_tags() == {'1.0'}
-        self.mgr._invoke('checkout', '1.0')
-        assert self.mgr.get_tags() == {'1.0'}
+        repo = vcs.Git('.')
+        assert repo.get_tags() == set()
+        repo._invoke('tag', '-am', "Tagging 1.0", '1.0')
+        assert repo.get_tags() == {'1.0'}
+        repo._invoke('checkout', '1.0')
+        assert repo.get_tags() == {'1.0'}
 
 
 class TestParseVersion:
@@ -47,9 +42,9 @@ class TestParseVersion:
 @pytest.mark.usefixtures("git_repo")
 class TestRevisionTimestamp:
     def test_tagged_rev_timestamp(self):
-        mgr = vcs.Git('.')
-        mgr._invoke('tag', '-am', 'tagging 1.0', '1.0')
-        assert mgr.get_timestamp('1.0').date() == datetime.date.today()
+        repo = vcs.Git('.')
+        repo._invoke('tag', '-am', 'tagging 1.0', '1.0')
+        assert repo.get_timestamp('1.0').date() == datetime.date.today()
 
 
 class TestIsolation:
